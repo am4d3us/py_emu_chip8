1d385f7ad9d153272feb615a83107f4463b0ba60
am4d3us <190080626+am4d3us@users.noreply.github.com>
2026-01-22 11:42:19 +0100

    root: stack impl & 00E0-7xkk impl

--- main.py
+++ main.py
@@ -1,81 +1,93 @@
 import numpy as np
 
 
+class Stack:
+    def __init__(self, size):
+        self.stack = np.zeros(size, dtype=np.uint8)
+        self.top = size - 1
+        self.pointer = 0
+
+    def push(self, value: np.uint8) -> np.uint8 | None:
+        if self.full():
+            return None
+
+        self.stack[self.pointer] = value
+        self.pointer += 1
+
+        return self.pointer
+
+    def pop(self) -> np.uint8 | None:
+        if self.empty():
+            return None
+
+        value = self.stack[self.pointer]
+        self.pointer -= 1
+        return value
+
+    def full(self) -> bool:
+        return self.pointer == self.top
+
+    def empty(self) -> bool:
+        return self.pointer == 0
+
+
 class Emulator:
-        """
-        An emulator for the CHIP-8 interpreted programming language.
-
-        Memory Specification:
-        ---
-        Commonly implemented on 4K systems with 4096 8-bit memory locations,
-        i.e. RAM (Random-Access Memory), sectioned according to the diagram below:
-
-        +---------------+= 0xFFF (4095) End of Chip-8 RAM
-        |               |
-        |               |
-        |               |
-        |               |
-        |               |
-        | 0x200 to 0xFFF|
-        |     Chip-8    |
-        | Program / Data|
-        |     Space     |
-        |               |
-        |               |
-        |               |
-        +- - - - - - - -+= 0x600 (1536) Start of ETI 660 Chip-8 programs
-        |               |
-        |               |
-        |               |
-        +---------------+= 0x200 (512) Start of most Chip-8 programs
-        | 0x000 to 0x1FF|
-        | Reserved for  |
-        |  interpreter  |
-        +---------------+= 0x000 (0) Start of Chip-8 RAM
-
-
-        Register Specification:
-        ---
-        16 8-bit GPIO (General Purpose Input/Output) registers, referred to as
-        'Vx', where 'x' is a hexadecimal digit. The 'VF' is a special register,
-        however, as it doubles as a flag and shall be avoided.
-
-        One 16-bit register, referred to as 'I', where only the 12 lowest bits
-        are utilized, used to store memory addresses.
-
-
-        Timer Specification:
-        ---
-        Two timers, both of which actively count down at 60 hertz whenever the
-        respective associated register is non-zero until they reach zero:
-        - Delay: intended to be used for timing game-events. (READ/WRITE)
-        - Sound: intended to be used for sound effects. (WRITE)
-
-
-        Input Specification:
-        ---
-        Hex keyboard with 16 keys ranging from 0 to F.
-        ...
-
-
-        Graphics Specification:
-        ---
-        ...
-        """
-
     def __init__(self):
         self.memory = np.zeros(4096, dtype=np.uint8)
         self.gpio = np.zeros(16, dtype=np.uint8)
-        self.index = 0 # The 'I' register.
+
+        self.index = 0  # The 'I' register.
+        self.pc = 0
+        self.opcode = 0
+
+        self.stack = np.zeros(16, dtype=np.uint8)
+        self.sp = 0
 
         self.timer_delay = 0
         self.timer_sound = 0
 
         self.keyboard = np.zeros(16, dtype=np.uint8)
-        self.display = np.zeros((32, 64), dtype=np.uint8)
+        self.display = np.zeros((64, 32), dtype=np.uint8)
 
+    def _get_x(self):
+        return (self.opcode >> 8) & 0xF
 
+    def _get_y(self):
+        return (self.opcode >> 4) & 0xF
 
+    def _get_kk(self):
+        return self.opcode & 0xFF
+
+    def _00E0(self):
+        self.display = np.zeros_like(self.display)
+
+    def _00EE(self):
+        self.pc = self.stack.pop()
+
+    def _1nnn(self):
+        self.pc = self.opcode & 0x0FFF
+
+    def _2nnn(self):
+        self.stack.push(self.pc)
+        self._1nnn(self.opcode)
+
+    def _3xkk(self):
+        if self.gpio[self._get_x()] == self._get_kk():
+            self.pc += 2
+
+    def _4xkk(self):
+        if self.gpio[self._get_x()] != self._get_kk():
+            self.pc += 2
+
+    def _5xy0(self):
+        if self.gpio[self._get_x()] == self.gpio[self._get_y()]:
+            self.pc += 2
+
+    def _6xkk(self):
+        self.gpio[self._get_x()] = self._get_kk()
+
+    def _7xkk(self):
+        self.gpio[self._get_x()] += self._get_kk()
 
 
 def main():
